d8734fdb95bfc46947686c41d6ae018ed6c5253a
Isaac Kogan <isaacikogan@gmail.com>
2022-04-08 17:45:16 -0400

    Let the hackathon begin!

---
+++ website/src/components/app.js
@@ -0,0 +1,12 @@
+import {Route, Routes} from "react-router-dom";
+
+const Main = () => {
+
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+
+        </Routes>
+    )
+
+}
---
+++ website/src/index.js
@@ -0,0 +1,9 @@
+const {createRoot} = require("react-dom");
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+
+
+)
